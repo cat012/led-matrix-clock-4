e0c34f2b0d9a72f1d6a8b6d5edba8d7d064a685f
cat012 <vkopenbox@yandex.ru>
2019-10-11 22:41:58 +0300

    file upload

---
+++ Inc/i2c.h
@@ -0,0 +1,46 @@
+// i2c.h
+//
+// Software I2C
+//
+// 26-05-2019
+//
+
+
+#ifndef I2C_H_
+#define I2C_H_
+
+
+#include "stm32f0xx_hal.h"
+
+
+#define I2C_PORT     GPIOA
+#define I2C_SDA_PIN  GPIO_PIN_10
+#define I2C_SCL_PIN  GPIO_PIN_9
+
+
+#define SDA_PULL  (HAL_GPIO_WritePin(I2C_PORT, I2C_SDA_PIN , GPIO_PIN_SET))
+#define SDA_NULL  (HAL_GPIO_WritePin(I2C_PORT, I2C_SDA_PIN , GPIO_PIN_RESET))
+#define SDA_STAT  (HAL_GPIO_ReadPin(I2C_PORT, I2C_SDA_PIN ))
+
+#define SCL_PULL  (HAL_GPIO_WritePin(I2C_PORT, I2C_SCL_PIN , GPIO_PIN_SET))
+#define SCL_NULL  (HAL_GPIO_WritePin(I2C_PORT, I2C_SCL_PIN , GPIO_PIN_RESET))
+
+
+#define I2C_DELAY  asm("nop")
+
+
+#define I2C_ACK   0
+#define I2C_NACK  1
+
+
+void i2c_init(void);
+void i2c_start(void);
+void i2c_stop(void);
+void i2c_send_byte(uint8_t data);
+uint8_t i2c_read_byte(uint8_t ack);
+
+
+
+#endif  //end of I2C_H_
+
+
---
+++ Src/max7219.c
@@ -0,0 +1,230 @@
+// file: max7219.c
+// 04-10-2019
+
+
+#include "max7219.h"
+
+
+uint8_t matrixbuff[MATRIX_BUFF_SIZE]={0};
+
+uint8_t matrixtempbuff[MAX7219_NUM*2]={0};
+
+uint8_t matrixshiftbuff[MATRIX_SHIFT_BUFF_SIZE]={0};
+
+
+
+
+
+#define MATRIX_BUFF_PIXEL_ON(x,y)   (REG_BIT_SET(matrixbuff[(((x)%(MAX7219_NUM*8))+(((y)%8)*(MAX7219_NUM*8)))/8],7-((x)%8)))
+#define MATRIX_BUFF_PIXEL_OFF(x,y)  (REG_BIT_CLR(matrixbuff[(((x)%(MAX7219_NUM*8))+(((y)%8)*(MAX7219_NUM*8)))/8],7-((x)%8)))
+
+
+
+
+//-------------------------------------------------------------------------------------------------
+static inline void max7219_gpio_init(void)
+    {
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    HAL_GPIO_WritePin(MAX7219_PORT, MAX7219_CS | MAX7219_CLK | MAX7219_DIN, GPIO_PIN_RESET);
+    GPIO_InitStruct.Pin = MAX7219_CS | MAX7219_CLK | MAX7219_DIN;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(MAX7219_PORT, &GPIO_InitStruct);
+    }
+
+
+//-------------------------------------------------------------------------------------------------
+static void matrix_spi_write(uint8_t *data)
+    {
+    MAX7219_CS_PIN(GPIO_PIN_RESET);
+
+    for(uint8_t i=0; i<MAX7219_NUM*2; i++)
+        {
+        for(uint8_t mask=0x80; mask; mask>>=1)
+            {
+            (data[i] & mask) ? MAX7219_DIN_PIN(GPIO_PIN_SET) : MAX7219_DIN_PIN(GPIO_PIN_RESET); //data line
+            MAX7219_CLK_PIN(GPIO_PIN_SET);; //clock pulse
+            MAX7219_CLK_PIN(GPIO_PIN_RESET);;
+            }
+        }
+
+    MAX7219_CS_PIN(GPIO_PIN_SET);
+    }
+
+
+//-------------------------------------------------------------------------------------------------
+static void matrix_command(uint8_t addr, uint8_t data)
+    {
+    for(uint8_t i=0; i<MAX7219_NUM*2; i+=2)
+        {
+        matrixtempbuff[i]=addr;
+        matrixtempbuff[i+1]=data;
+        }
+    matrix_spi_write(matrixtempbuff);
+    }
+
+
+//-------------------------------------------------------------------------------------------------
+static void matrix_data(uint8_t *data)
+    {
+    for(uint8_t k=0; k<8; k++)
+        {
+        for(uint8_t i=0; i<MAX7219_NUM*2; i+=2)
+            {
+            matrixtempbuff[i]=1+k;
+            matrixtempbuff[i+1]=*data++;
+            }
+        matrix_spi_write(matrixtempbuff);
+        }
+    }
+
+
+//------------------------------------------------------------------------------------------------
+void matrix_clear(void)
+    {
+    for(uint8_t i=0; i<MATRIX_BUFF_SIZE; i++) matrixbuff[i]=0;
+    }
+
+
+//------------------------------------------------------------------------------------------------
+void matrix_fill(void)
+    {
+    for(uint8_t i=0; i<MATRIX_BUFF_SIZE; i++) matrixbuff[i]=0xff;
+    }
+
+
+//------------------------------------------------------------------------------------------------
+void matrix_brightness(uint8_t val)
+    {
+    matrix_command(MAX7219_MAP_INTENSITY, (val & MAX7219_INTENSITY_MASK));
+    }
+
+
+//-------------------------------------------------------------------------------------------------
+void matrix_init(void)
+    {
+    max7219_gpio_init();
+
+    matrix_command(MAX7219_MAP_SHUTDOWN, 0x00);    //shutdown mode
+    matrix_command(MAX7219_MAP_DECODEMODE, 0x00);  //no decode
+    matrix_command(MAX7219_MAP_INTENSITY, 0x00);   //minimum brightness
+    matrix_command(MAX7219_MAP_SCANLIMIT, 0x07);   //display all digits
+    matrix_command(MAX7219_MAP_DISPTEST, 0x00);    //normal operation
+
+    matrix_clear();
+    matrix_data(matrixbuff);
+
+    matrix_command(MAX7219_MAP_SHUTDOWN, 0x01);      //normal operation
+    }
+
+
+//------------------------------------------------------------------------------------------------
+void matrix_update(void)
+    {
+    matrix_data(matrixbuff);
+    matrix_clear();
+    }
+
+
+//-------------------------------------------------------------------------------------------------
+void matrix_char_small(uint8_t xpos, uint8_t code)
+    {
+    uint8_t c=0;
+
+    if(code>=48 && code<=58) c=code-48;
+    else if(code==32) c=11;
+    else if(code==43) c=12;
+    else if(code==45) c=13;
+    else c=14;
+
+    for(uint8_t y=0; y<7; y++)
+        {
+        for(uint8_t x=0; x<3; x++)
+            {
+        	if(REG_BIT_IS_SET(font3x7[c*8+y],(2-x))) MATRIX_BUFF_PIXEL_ON((uint8_t)(x+xpos),y);
+            }
+        }
+    }
+
+
+//-------------------------------------------------------------------------------------------------
+void matrix_print_small(uint8_t xpos, const char *str)
+    {
+    for(; *str; xpos+=4) matrix_char_small(xpos,*str++);
+    }
+
+
+//------------------------------------------------------------------------------------------------
+void matrix_clear_shift(void)
+    {
+    for(uint16_t i=0; i<MATRIX_SHIFT_BUFF_SIZE; i++) matrixshiftbuff[i]=0x00;
+    }
+
+
+//------------------------------------------------------------------------------------------------
+void matrix_copy_shift(uint16_t cpos)
+    {
+    if(cpos>((MATRIX_SHIFT_BUFF_SIZE-MATRIX_BUFF_SIZE)-1)) cpos=0;
+
+    for(uint8_t x=0; x<MATRIX_BUFF_SIZE; x++)
+        {
+        for(uint8_t y=0; y<8; y++)
+            {
+            if(REG_BIT_IS_SET(matrixshiftbuff[x+cpos],y)) MATRIX_BUFF_PIXEL_ON(x,y);
+            }
+        }
+    }
+
+
+//------------------------------------------------------------------------------------------------
+void matrix_pixel_shift(uint16_t xpos, uint8_t ypos, uint8_t on)
+    {
+    if((ypos<=7) && (xpos<=(MATRIX_SHIFT_BUFF_SIZE-1)))
+        {
+        uint16_t x = xpos;
+        uint8_t y = ypos;
+        if(on) REG_BIT_SET(matrixshiftbuff[x],y);
+        else REG_BIT_CLR(matrixshiftbuff[x],y);
+        }
+    }
+
+
+//------------------------------------------------------------------------------------------------
+void matrix_char_shift(uint16_t xpos, uint8_t code)
+    {
+    uint8_t c=0;
+
+    if(code>=48 && code<=58) c=code-48;
+    else if(code>=65 && code<=90) c=(code-65)+15;
+    else if(code>=192 && code<=223) c=(code-192)+42;
+    else if(code==32) c=11;
+    else if(code==43) c=12;
+    else if(code==45) c=13;
+    else if(code==46) c=14;
+    else c=41;
+
+    for(uint8_t y=0; y<8; y++)
+        {
+        for(uint8_t x=0; x<5; x++)
+            {
+            if(REG_BIT_IS_SET(font5x8[c*8+y],(4-x))) matrix_pixel_shift(x+xpos,y,1);
+            }
+        }
+    }
+
+
+//------------------------------------------------------------------------------------------------
+void matrix_print_shift(uint16_t xpos, const char *str)
+    {
+    for(; *str; xpos+=7) matrix_char_shift(xpos,*str++);
+    }
+
+
+//-------------------------------------------------------------------------------------------------
+void matrix_print_shift_compact(uint8_t xpos, const char *str)
+    {
+    for(; *str; xpos+=6) matrix_char_shift(xpos,*str++);
+    }
+
+
